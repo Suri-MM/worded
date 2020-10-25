4f7b354d684a23b8ce6dbe4200efd4b64928843e
Suri-MM <30298787+Suri-MM@users.noreply.github.com>
2020-10-26 03:16:12 +0530

    Updates

---
+++ precache-manifest.aba8dd3fb9e1565392e5ee587715044a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f964deeb56119ef8dfa72a19808c86f7",
+    "url": "/worded/index.html"
+  },
+  {
+    "revision": "98c2e5b400beaf4ba499",
+    "url": "/worded/static/css/main.8c60b743.chunk.css"
+  },
+  {
+    "revision": "e8afb4cae65399810cea",
+    "url": "/worded/static/js/2.b0382c0c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/worded/static/js/2.b0382c0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98c2e5b400beaf4ba499",
+    "url": "/worded/static/js/main.c99f4317.chunk.js"
+  },
+  {
+    "revision": "5cda5e48a4b581f51d73",
+    "url": "/worded/static/js/runtime-main.f1f1ad8a.js"
+  },
+  {
+    "revision": "5f32d873fd187607b5d3ab970233a8d5",
+    "url": "/worded/static/media/Acme-Regular.5f32d873.woff"
+  },
+  {
+    "revision": "2e5e2b8c03d09b1f548880d7bf7b58df",
+    "url": "/worded/static/media/Dreamscape.2e5e2b8c.woff"
+  },
+  {
+    "revision": "9bc0cf7d3260b31d7e749df9187a5d4b",
+    "url": "/worded/static/media/JosefinSans-Regular.9bc0cf7d.woff"
+  },
+  {
+    "revision": "467506ede59e0f35bfaf5edc5b1f3630",
+    "url": "/worded/static/media/OpenSans-LightItalic.467506ed.woff"
+  },
+  {
+    "revision": "51960e2f883937fd97470ec5519fe179",
+    "url": "/worded/static/media/OverpassMono-Regular.51960e2f.woff"
+  },
+  {
+    "revision": "abc6d406576c94f5fc9488e1aeff8f5e",
+    "url": "/worded/static/media/PlayfairDisplay-Regular.abc6d406.woff"
+  },
+  {
+    "revision": "5789aa34b6eb097e093b9ceae8e8750d",
+    "url": "/worded/static/media/museosanscyrl_100_italic.5789aa34.woff"
+  },
+  {
+    "revision": "0c2615115789832ee22de99c3cdfd889",
+    "url": "/worded/static/media/wordshavepower.0c261511.gif"
+  }
+]);
